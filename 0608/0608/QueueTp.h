aa75dc743a4cfc652db5886d18ce57d967b33f81
KwonYeongmin <kym60002@daum.net>
2021-06-08 15:03:52 +0900

    1

--- 0608/0608/QueueTp.h
+++ 0608/0608/QueueTp.h
@@ -1,111 +1,62 @@
-#pragma once
-#ifndef QUEUEUP_H_
-#define QUEUEUP_H_
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
 
 template<class Type>
-class Worker 
+class Queue 
 {
 private:
-	std::string fullname;
-	long id;
-protected:
-	virtual void Data() const;
-	virtual void Get();
-public: 
-	Worker() :fullname("noname"), id(0L) {}
-	Worker(const std::string &s,long n) :fullname(s), id(n) {}
-	virtual void ~Worker() = 0;
-	virtual void Set() = 0;
-	virtual void Show() const = 0;
-};
-
-template<class Type>
-class Waiter :virtual public Worker 
-{
-private: int panache;
-protected: 
-	void Data() const;
-	void Get();
-public: 
-	Waiter() :Worker(),panache(0) {}
-	Waiter(const std::string &s,long n,int p=0):Worker(s,n),panache(p){}
-	void Set();
-	void Show() const;
-};
-
-
-template<class Type>
-class Singer :virtual public Worker
-{
-protected:
-	enum
-	{other,alto,contralto,soprano,bass,baritone,tenor};
-	enu{ Vtypes = 7};
-	void Data() const;
-	void Get();
-private:
-	static char *pv[Vtypes];
-	int voice;
+	enum { SIZE = 10 };
+	int size;
+	Type *items;
+	int top;
 public:
-	Singer() :Worker(), voice(other) {}
-	Singer(const std::string &s, long n, int v = other) :Worker(s, n), voice(v) {}
-	Singer(const Worker &wk, int v = other) :Worker(s,n),voice(v) {}
-	void Set();
-	void Show() const;
-};
-
-template<class Type>
-class SingingWaiter :public Singer,public Waiter
-{
-protected:
-	void Data() const;
-	void Get();
-public:
-	SingingWaiter() {}
-	
-	SingingWaiter(const Worker &wk, int p = 0,int v=other)
-	:Worker(s,n),Waiter(s,n,p),Singer(s,n,v){}
-	
-	SingingWaiter(const Waiter&wt,  int v = other)
-	:Worker(wt), Waiter(wt), Singer(wt, v) {}
-	SingingWaiter(const Singer&wt, int p = 0)
-		:Worker(wt), Waiter(wt,p), Singer(wt, v) {}
-	void Set();
-	void Show() const;
+	Queue(int ss = SIZE); //여기에 문제잇음 ㅜㅜ
+	Queue(const Queue &q);
+	~Queue() { delete[] items; };
+	bool isempty() { return top == 0; };
+	bool isfull() { return top == size; }
+	bool Enque(const Type &item);
+	bool Deque(Type &item);
+	Queue & operator= (const Queue & q);
 };
 
 template <class Type>
-Worker<Type> ::~Worker() {}
-
-template <class Type>
-void Worker<Type> ::Data() const 
+Queue <Type>::Queue(int ss) : stacksize = ss,top(0)
 {
-	cout << "사원 이름: " << fullname << endl;
-	cout << "사원 번호: " << id ;
+	items = new Type[stacksize];
+}
+template <class Type>
+Queue <Type>::Queue(const Queue &q) 
+{
+	size = q.size;
+	top = q.top;
+	items = new Type[size];
+	for (int i = 0; i < top; i++) items[i] = q.items[i];
+}
+template <class Type>
+bool Queue<Type>::Enque(const Type &item)
+{
+	if (top < size) { items[top++] = item; return true; }
+	else return false;
 }
 
 template <class Type>
-void Worker<Type>::Get() 
+bool Queue<Type>::Deque(Type &item)
 {
-	getline(cin, fullname); 
-	cout << "사원번호를 입력: ";
-	cin >> id;
-	while (cin.get() != '\n') continue;
+	if (top > 0) { item = items[--top]; return true; }
+	else return false;
 }
-
-
 template <class Type>
-void Worker<Type>::Set()
+Queue<Type>&Queue<Type>::operator= (const Queue<Type> &q)
 {
-	getline(cin, fullname);
-	cout << "사원번호를 입력: ";
-	cin >> id;
-	while (cin.get() != '\n') continue;
+
 }
 
-#endif // !QUEUEUP_H_
+
+#endif // !QUEUE_H_
 
