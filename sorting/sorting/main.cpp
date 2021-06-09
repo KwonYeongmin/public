72110078796ceea58572a2514a68a1fed191ee10
KwonYeongmin <kym60002@daum.net>
2021-06-09 17:48:28 +0900

    0609

--- sorting/sorting/main.cpp
+++ sorting/sorting/main.cpp
@@ -33,9 +33,10 @@ int main()
 	int cnt_swap=0;
 	cout << "< quick sort >" << endl;
 	q_timer.start();
-	quick(arr, mark, size);
+	quick(arr, mark, size,&cnt);
 	q_timer.stop();
 	cout << "걸린시간: " << q_timer.getElapsedTime() << "ms" << endl;
+	cout << "비교를 " << cnt << "회 수행했습니다." << endl;
 	delete[] arr;
 	delete[] mark;
 
--- sorting/sorting/quick.cpp
+++ sorting/sorting/quick.cpp
@@ -1,36 +1,53 @@
 #include "main.h"
-void quick(int arr[],char mark[],int size) 
+//재귀함수
+void quick(int arr[], char mark[], int size,int *count)
 {
 	int i, j, k;
-	int count = 0; int count_swap=0;
+	//int count = 0; int count_swap=0;
 	//초기 피벗 설정
-	int px = size/2;
+	int px = size / 2;
 	int pl = 0, pr = size - 1;
 
-	//for (j = 0; j < ) 
-	//{
 		for (i = 0; i < size; i++)
 		{
-			mark[pl] = ' '; mark[pr] = ' ';
-			if (arr[pl] >= arr[px]) { mark[pl] = '+'; }
-			else { pl++; mark[pl] = '-'; }
+			mark[pl] = ' '; mark[pr] = ' '; mark[px] = '*';
+			if (px < i) 
+			{
+				if (arr[pl] >= arr[px]) { mark[pl] = '-'; }
+				else { pl++; mark[pl] = '+'; }
 
-			if (arr[pr] <= arr[px]) { mark[pr] = '+'; }
-			else { pr--; mark[pr] = '-'; }
+				if (arr[pr] <= arr[px]) { mark[pr] = '-'; }
+				else { pr--; mark[pr] = '+'; }
+
+				quick(arr, mark, i,count);
+			}
+			else 
+			{
+				if (arr[pl] >= arr[px]) { mark[pl] = '-'; }
+				else { pl++; mark[pl] = '+'; }
+
+				if (arr[pr] <= arr[px]) { mark[pr] = '-'; }
+				else { pr--; mark[pr] = '+'; }
+
+				quick(arr, mark, i, count);
+			}
+			
 
 			swap(int, arr[pl], arr[pr]);
-			count_swap++;
+			//count_swap++;
 			//출력
 			for (k = 0; k < size; k++) cout << mark[k] << " ";
 			cout << endl;
 			Print(arr, size);
 		}
-
-		count++;
-		cout << "-----------------------" << endl;
-	//}
+		
+	//	cout << "px: " << px << endl;
+	*count++;
+	//	cout << "-----------------------" << endl;
 	Print(arr, size);
-	cout << "비교를 " << count << "회 수행했습니다." << endl;
-	cout << "교환을 " << count_swap << "회 수행했습니다." << endl;
+	//cout << "비교를 " << *count << "회 수행했습니다." << endl;
+	//cout << "교환을 " << count_swap << "회 수행했습니다." << endl;
 }	
+
+
 
