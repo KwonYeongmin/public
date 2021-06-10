d49cd816e148aba3a04638f7d85ea40712b3c57f
KwonYeongmin <kym60002@daum.net>
2021-06-10 20:37:34 +0900

    ffds

--- sorting/sorting/quick.cpp
+++ sorting/sorting/quick.cpp
@@ -4,115 +4,64 @@ void quick(int arr[], char mark[], int size,int px ,int pl,int pr)
 {
 	int i, j, k;
 	int count = 0; int count_swap=0;
-	//int pl = 0; int pr = size-1;
-	//px = (right - left) / 2
-	cout << "px: "<< px <<" , arr[px] : " << arr[px] << endl;
+	int left=pl, right=pr, x=px;
+
+	//cout << "px: "<< x <<" , arr[px] : " << arr[x] << endl;
 
 	for (j = 0; j < size - 1; j++) 
 	{
 			//mark[pl] = ' '; mark[pr] = ' '; mark[px] = '*';
-		if (arr[pl] < arr[px]) pl++; 
-		if (arr[pr] > arr[px]) pr--; 
-		swap(int, arr[pl], arr[pr]);	
-		Print(arr, size);
+		if (arr[left] < arr[x]) left ++;
+		if (arr[right] > arr[x]) right--;
+		swap(int, arr[left], arr[right]);
 	}
-	pl--; pr++;
-	cout << "px: " << px << " , arr[px] : " << arr[px] << endl;
+	left--; right++;
+	cout << "px: " << x << " , arr[px] : " << arr[x] << endl;
 	Print(arr, size);
-	if (pl < px)
-	{
-		int right = pl;
-		int left = 0;
-		cout << "哭率" << endl;
+
+
+	if (left < px)
+	{	
+		int r = left;
+		int l = 0;
+		//quick(arr, mark, r-l, (r-l)/2, l, r);
+		//cout << "哭率" << endl;
 		//for (i = 0; i < pl+1; i++) { cout << "arr[" << i << "] = " << arr[i] << " "; }
 		//cout << endl;
-		px = (right - left) / 2;
-		int s = right - left +1;
+		x = (r - l) / 2;
+		int s = r- l;
 		//cout << "pl: " << pl << " , pr: " << pr << " , px: " << px<<" , size: "<<size<<endl;
-		for (j = 0; j < size - 1; j++) 
+		for (j = 0; j < s- 1; j++) 
 		{
-			if (arr[left] < arr[px]) left++;
-			if (arr[right] > arr[px]) right--;
-			swap(int, arr[left], arr[right]);
-		}
-		Print(arr, s);
-	}
-	if (pr > px)
-	{
-		int right = size-1;
-		int left = pr-1;
-		for (i = pr-1; i < size; i++) { cout << "arr[" << i << "] = " << arr[i] << " "; }
-		cout << endl;
-		cout << "坷弗率" << endl;
-		px = (right - left) / 2;
-		int s = right - left + 1;
-
-		for (j = 0; j < size - 1; j++)
-		{
-			if (arr[left] < arr[px]) left++;
-			if (arr[right] > arr[px]) right--;
-			swap(int, arr[left], arr[right]);
+			if (arr[l] < arr[x]) l++;
+			if (arr[r] > arr[x]) r--;
+			swap(int, arr[l], arr[r]);
 		}
 		//Print(arr, s);
 	}
-	/*
-		//cout<<"pl: " << pl<<endl;
-		//quick(arr, mark, px - pl, ((px - pl) / 2),0,pl); 
-		size = px - pl;
-		px = ((px - pl) / 2);
-		pl = 0;
-		pr = pl;
-		//cout << "px: " << px << " , arr[px] : " << arr[px] << endl;
-		for (j = 0; j < size - 1; j++)
-		{
-			//mark[pl] = ' '; mark[pr] = ' '; mark[px] = '*';
-			if (arr[pl] < arr[px]) pl++;
-			if (arr[pr] > arr[px]) pr--;
-			swap(int, arr[pl], arr[pr]);
-		}
-		pl--; pr++;
-		Print(arr, size);
-	//*/
-	
-	//else if (pr > px)
-	//{
-	//	cout<<"pr: " << pr << endl;
-		//quick(arr, mark, pr - px, px + ((pr - px) / 2),pr,size-1);
-	/*
-		size = pr - px;
-		px = px + ((pr - px) / 2);
-		pl = pr;
-		pr = size - 1;
-	//	cout << "px: " << px << " , arr[px] : " << arr[px] << endl;
-
-		for (j = 0; j < size - 1; j++)
-		{
-			//mark[pl] = ' '; mark[pr] = ' '; mark[px] = '*';
-			if (arr[pl] < arr[px]) pl++;
-			if (arr[pr] > arr[px]) pr--;
-			swap(int, arr[pl], arr[pr]);
-		}
-		pl--; pr++;
-		//Print(arr, size);*/
-//	}
-	
-
-	
-	/*
-
-	if (pl < px)
+	if (right > px)
 	{
-		//px = (px - pl) / 2;
-		quick(arr, mark, px - pl, (px - pl) / 2);
-	}
-	if (pr > px)
-	{
-			//px = px + ( (pr - px) / 2 );
-		quick(arr, mark, pr - px, px + ((pr - px) / 2));
-	}
-	*/
+		int r = size-1;
+		int l = right-1;
+		//quick(arr, mark, r - l+1, l + (r - l) / 2, l, r);
 		
+		//cout << "坷弗率" << endl;
+		x = l+ (r - l) / 2;
+		int s = r - l + 1;
+		for (j = 0; j < s-1; j++)
+		{
+			if (arr[l] < arr[x]) l++;
+			if (arr[r] > arr[x]) r--;
+			swap(int, arr[l], arr[r]);
+			/*
+			for (i = l; i < r + 1; i++) cout << arr[i] << " ";
+			cout << endl;*/
+		}
 		cout << endl;
+	}
+
+		
+	cout << endl;
 	cout << "-----------------------" << endl;
 	Print(arr, size);
 	cout << "-----------------------" << endl;
