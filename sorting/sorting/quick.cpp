6edd192aaa8102eb133c22dae434916e364a6e1c
KwonYeongmin <kym60002@daum.net>
2021-06-12 03:07:14 +0900

    home

--- sorting/sorting/quick.cpp
+++ sorting/sorting/quick.cpp
@@ -4,26 +4,115 @@
 
 void quick(int arr[], char mark[], int size, int left, int right)
 {
-	int px = size / 2;
-	int s = size;
-	
-	int i=0;
+	int px =arr[ size / 2];
+	int r = right; int l = left;
+	int s = r - l + 1;
+	int i=0,j;
 
 	//Print(arr, size);
-	cout << arr[px] << endl;
+
 	while (1)
-	{	//px가 변하지 않도록 고정해주기
-		while (arr[left] < arr[px]) left++; 
-		while (arr[right] > arr[px]) right--;
-		swap(int, arr[left], arr[right]);
-		cout<<"arr[px] : "<< arr[px] <<" , arr[left] : " << arr[left] << " ,arr[right] :" << arr[right] << endl;
-		
-		Print(arr, size);
+	{	
+		while (arr[l] < px) l++; 
+		while (arr[r] > px) r--;
+		swap(int, arr[l], arr[r]);
+		//cout << "left : " << left << " , right : " << right << endl;
 		i++;
-		if (i > size-1) break;
+		if (i > s/2) break;
+	}
+
+	Print(arr, size);
+	if (s > 1) 
+	{
+		//왼쪽
+		cout << "r: " << r << " , l: " << l << endl;
+		if (l > left)
+		{
+			int l_ = 0; int r_ = l - 1;
+			cout << arr[l_] << " ~ " << arr[r_] << endl;
+			int s_ = r_ - l_ + 1;
+			px = arr[s_ / 2];
+			cout << px << endl;
+			quick(arr, mark, s_, l_, r_);
+		}
+		Print(arr, size);
+		//오른쪽
+		r = 7; l = 7;
+		cout << "r: " << r << " , l: " << l << endl;
+		if (r < right)
+		{
+			int l_ = r; int r_ = s - 1;
+			int s_ = r_ - l_ + 1;
+			px = arr[s_ / 2];
+			quick(arr, mark, s_, l_, r_);
+		}
+		Print(arr, size);
+		cout << "--------------------------" << endl;
+		Print(arr, size);
+	}
+	
+	/*
+	int r = left - 1;
+	int l = 0;
+	s = r- l + 1;
+	px= arr[size / 2];
+	while (1)
+	{
+		while (arr[l] < px) l++;
+		while (arr[r] > px) r--;
+		swap(int, arr[l], arr[r]);
+		i++;
+		if (i > s / 2) break;
 	}
 	cout << "--------------------------" << endl;
 	Print(arr, size);
+
+	r = size - 1;
+	l = right;
+	s = r - l + 1;
+	px = arr[size / 2];
+	while (1)
+	{
+		while (arr[l] < px) l++;
+		while (arr[r] > px) r--;
+		swap(int, arr[l], arr[r]);
+		i++;
+		if (i > s / 2) break;
+	}
+	cout << "--------------------------" << endl;
+	Print(arr, size);
+
+	int r_ = left - 1;
+	int l_ = 0;
+	s = r - l + 1;
+	px = arr[size / 2];
+	while (1)
+	{
+		while (arr[l] < px) l++;
+		while (arr[r] > px) r--;
+		swap(int, arr[l], arr[r]);
+		i++;
+		if (i > s / 2) break;
+	}
+	cout << "--------------------------" << endl;
+	Print(arr, size);
+
+	r_ = size - 1;
+	l_ = right;
+	s = r - l + 1;
+	px = arr[size / 2];
+	while (1)
+	{
+		while (arr[l] < px) l++;
+		while (arr[r] > px) r--;
+		swap(int, arr[l], arr[r]);
+		i++;
+		if (i > s / 2) break;
+	}
+	cout << "--------------------------" << endl;
+	Print(arr, size);
+
+	*/
 }
 
 
